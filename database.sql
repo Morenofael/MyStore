8bb4f7d11b162cc03df8f908e33b179817e4d927
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2024-07-29 19:48:19 -0300

    add base usuario e endereco

---
+++ database.sql
@@ -0,0 +1,25 @@
+CREATE DATABSE mystore;
+USE mystore;
+CREATE TABLE usuario(
+	id INT AUTO_INCREMENT NOT NULL,
+	nome VARCHAR(256),
+	email VARCHAR(100),
+	senha VARCHAR(256),
+	cpf VARCHAR(11),
+	telefone VARCHAR(13),
+	data_nascimento DATE,
+	constraint pk_usuario primary key (id)
+);
+CREATE TABLE endereco(
+	id INT AUTO_INCREMENT NOT NULL
+	rua VARCHAR(45),
+	numero VARCHAR(45),
+	bairro VARCHAR(256),
+	cidade VARCHAR(256),
+	estado VARCHAR(2),
+	país VARCHAR(3),
+	CEP VARCHAR(8),
+	id_usuario INT
+);
+ALTER TABLE endereco ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id);
+
