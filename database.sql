309056c1706811395676b9264c2198a73c9a08e7
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-08-12 13:31:04 -0300

    usar plural

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE mystore;
 USE mystore;
-CREATE TABLE usuario(
+CREATE TABLE usuarios(
 	id INT AUTO_INCREMENT NOT NULL,
 	nome VARCHAR(256),
 	email VARCHAR(100),
@@ -9,12 +9,12 @@ CREATE TABLE usuario(
 	cpf VARCHAR(11),
 	telefone VARCHAR(13),
 	data_nascimento DATE,
-	nivel_acesso TINYINT(1) DEFAULT 1,/*0 é usuário, 1 é administrador*/
+	nivel_acesso TINYINT(1),/*0 é usuário, 1 é administrador*/
 	situacao TINYINT(1),/*0 é inativo, 1 é ativo*/
 	constraint pk_usuario primary key (id)
 );
 
-CREATE TABLE endereco(
+CREATE TABLE enderecos(
 	id INT AUTO_INCREMENT NOT NULL,
 	rua VARCHAR(45),
 	numero VARCHAR(45),
@@ -26,9 +26,9 @@ CREATE TABLE endereco(
 	id_usuario INT,
 	constraint pk_endereco primary key (id)
 );
-ALTER TABLE endereco ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id);
+ALTER TABLE endereco ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios (id);
 
-CREATE TABLE produto(
+CREATE TABLE produtos(
 	id INT AUTO_INCREMENT NOT NULL,
 	id_usuario INT,
 	nome VARCHAR(256),
@@ -36,9 +36,9 @@ CREATE TABLE produto(
 	descricao TEXT,
 	constraint pk_produto primary key (id)
 );
-ALTER TABLE produto ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuario (id);
+ALTER TABLE produto ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios (id);
 
-CREATE TABLE pedido(
+CREATE TABLE pedidos(
 	id INT AUTO_INCREMENT NOT NULL,
 	data DATE,
 	status ENUM("I", "VV", "SE", "FE"),/*Se o produto foi para o correio, se chegou, etc...*/
@@ -48,24 +48,24 @@ CREATE TABLE pedido(
 	valor_total DOUBLE(5,2),
 	constraint pk_pedido primary key (id)
 );
-ALTER TABLE pedido ADD CONSTRAINT fk_comprador FOREIGN KEY (id_comprador) REFERENCES usuario (id);
-ALTER TABLE pedido ADD CONSTRAINT fk_vendedor FOREIGN KEY (id_vendedor) REFERENCES usuario (id);
+ALTER TABLE pedido ADD CONSTRAINT fk_comprador FOREIGN KEY (id_comprador) REFERENCES usuarios (id);
+ALTER TABLE pedido ADD CONSTRAINT fk_vendedor FOREIGN KEY (id_vendedor) REFERENCES usuarios (id);
 
-CREATE TABLE pedido_produto(
+CREATE TABLE pedidos_produtos(
 	id INT AUTO_INCREMENT NOT NULL,
 	id_pedido INT,
 	id_produto INT,
 	valor DOUBLE(5,2),
 	constraint pk_pedido_produto primary key (id)
 );
-ALTER TABLE pedido_produto ADD CONSTRAINT fk_pedido FOREIGN KEY (id_pedido) REFERENCES pedido (id);
-ALTER TABLE pedido_produto ADD CONSTRAINT fk_produto FOREIGN KEY (id_produto) REFERENCES produto (id);
+ALTER TABLE pedidos_produtos ADD CONSTRAINT fk_pedido FOREIGN KEY (id_pedido) REFERENCES pedidos (id);
+ALTER TABLE pedidos_produtos ADD CONSTRAINT fk_produto FOREIGN KEY (id_produto) REFERENCES produtos (id);
 
-CREATE TABLE avaliacao_produto(
+CREATE TABLE avaliacoes_produtos(
 	id INT AUTO_INCREMENT NOT NULL,
 	id_pedido INT,
 	estrelas INT,
 	comentario TEXT,
 	constraint pk_avaliacao_produto primary key (id)
 );
-ALTER TABLE avaliacao_produto ADD CONSTRAINT fk_pedido FOREIGN KEY (id_pedido) REFERENCES pedido (id);
+ALTER TABLE avaliacoes_produtos ADD CONSTRAINT fk_pedido FOREIGN KEY (id_pedido) REFERENCES pedidos (id);
