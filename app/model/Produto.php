1a64a228269e046657f78885172f38ff4ad9b95a
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-09-13 16:33:35 -0300

    add produto model

---
+++ app/model/Produto.php
@@ -0,0 +1,104 @@
+<?php 
+#Nome do arquivo: Produto.php
+#Objetivo: classe Model para Produto 
+
+class Brecho implements JsonSerializable {
+
+    private ?int $id;
+    private ?int $idBrecho;
+    private ?string $nome;
+    private ?string $descricao;
+    private ?float $preco;
+
+    public function jsonSerialize(): array {
+        return array("id" => $this->id,
+                     "idBrecho" => $this->idBrecho,
+                     "nome" => $this->nome,
+                     "descricao" => $this->descricao,
+                     "preco" => $this->preco);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idBrecho
+     */
+    public function getIdBrecho(): ?int
+    {
+        return $this->idBrecho;
+    }
+
+    /**
+     * Set the value of idBrecho
+     */
+    public function setIdBrecho(?int $idBrecho): self
+    {
+        $this->idBrecho = $idBrecho;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(?string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descricao
+     */
+    public function getDescricao(): ?string
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * Set the value of descricao
+     */
+    public function setDescricao(?string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of preco
+     */
+    public function getPreco(): ?float
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set the value of preco
+     */
+    public function setPreco(?float $preco): self
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+}
