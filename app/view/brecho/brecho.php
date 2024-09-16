34d5207b2ad9d763fd261b2f9ad95eb6541fb1ee
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-09-16 15:48:04 -0300

    update

--- app/view/brecho/brecho.php
+++ app/view/brecho/brecho.php
@@ -4,7 +4,6 @@
 
 require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
-require_once(__DIR__ . "/../../model/Brecho.php");
 $brecho = $dados["brecho"];
 ?>
 <h3 class="text-center">
