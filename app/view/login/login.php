fc89cd340fa4b7dc06a7a2f0943dbd906328e87d
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-09-27 16:42:10 -0300

    style login

--- app/view/login/login.php
+++ app/view/login/login.php
@@ -6,31 +6,34 @@ require_once(__DIR__ . "/../include/header.php");
 ?>
 <!-- TODO perguntar para o professor se não é melhor não usar o BASEURL-->
 <link rel="stylesheet" href="<?=BASEURL?>/view/login/login.css" media="all">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <section class="main">
 <h4>LOGIN</h4>
     <div class="form-container">
-
                 <!-- Formulário de login -->
                 <form id="frmLogin" action="./LoginController.php?action=logon" method="POST" >
-                    <div class="form-group">
+                    <div class="input-wrapper">
                         <label for="txtLogin">Email:</label>
-                        <input type="text" class="form-control" name="login" id="txtLogin"
-                            placeholder="Informe o email"
+                        <input type="text" class="" name="login" id="txtLogin"
+                            placeholder="Email"
                             value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>" />        
                     </div>
 
-                    <div class="form-group">
+                    <div class="input-wrapper">
                         <label for="txtSenha">Senha:</label>
-                        <input type="password" class="form-control" name="senha" id="txtSenha"
-                            maxlength="15" placeholder="Informe a senha"
+                        <input type="password" class="" name="senha" id="txtSenha"
+                            maxlength="15" placeholder="Senha"
                             value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />        
                     </div>
 
-                    <button type="submit" class="btn btn-success">Logar</button>
+                    <button type="submit" class="">Logar</button>
                 </form>
-                <a href="/app/controller/UsuarioController.php?action=create" class="btn btn-primary">Cadastre-se</a>
+                <div class="form-link-wrapper">
+                    <a href="#">Esqueceu sua senha?</a>
+                    <a href="/app/controller/UsuarioController.php?action=create" class="">Cadastre-se</a>
+                </div>
 
-        <div class="col-6">
+        <div class="col-12">
             <?php include_once(__DIR__ . "/../include/msg.php") ?>
         </div>
     </div>
