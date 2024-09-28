8940eede6805c7dbe2ded1f20001462f6a872eed
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-09-28 15:49:12 -0300

    style

--- app/view/usuario/form.php
+++ app/view/usuario/form.php
@@ -3,66 +3,63 @@
 #Objetivo: interface para listagem dos usuários do sistema
 
 require_once(__DIR__ . "/../include/header.php");
-require_once(__DIR__ . "/../include/menu.php");
 ?>
+<link rel="stylesheet" href="<?=BASEURL?>/view/usuario/usuario.css" media="all">
 
-<h3 class="text-center">
+<section class="main">
+<h4 class="text-center">
     <?php if($dados['id'] == 0) echo "Inserir"; else echo "Alterar"; ?> 
     Usuário
-</h3>
+</h4>
 
-<div class="container">
-    
-    <div class="row" style="margin-top: 10px;">
-        
-        <div class="col-6">
+<div class="form-container">
             <form id="frmUsuario" method="POST" 
                 action="<?= BASEURL ?>/controller/UsuarioController.php?action=save" >
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="txtNome">Nome:</label>
-                    <input class="form-control" type="text" id="txtNome" name="nome" 
+                    <input type="text" id="txtNome" name="nome" 
                         maxlength="70" placeholder="Informe o nome"
                         value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getNome() : ''); ?>" />
                 </div>
                 
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="txtEmail">Email:</label>
-                    <input class="form-control" type="email" id="txtEmail" name="email" 
+                    <input type="email" id="txtEmail" name="email" 
                         maxlength="345" placeholder="Informe o email"
                         value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getEmail() : ''); ?>"/>
                 </div>
 
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="txtSenha">Senha:</label>
-                    <input class="form-control" type="password" id="txtPassword" name="senha" 
+                    <input type="password" id="txtSenha" name="senha" 
                         maxlength="15" placeholder="Informe a senha"
                         value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getSenha() : ''); ?>"/>
                 </div>
 
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="txtConfSenha">Confirmação da senha:</label>
-                    <input class="form-control" type="password" id="txtConfSenha" name="conf_senha" 
+                    <input type="password" id="txtConfSenha" name="conf_senha" 
                         maxlength="15" placeholder="Informe a confirmação da senha"
                         value="<?php echo isset($dados['confSenha']) ? $dados['confSenha'] : '';?>"/>
                 </div>
                 
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="txtCpf">CPF:</label>
-                    <input class="form-control" type="text" id="txtCPF" name="cpf" 
+                    <input type="text" id="txtCpf" name="cpf" 
                         maxlength="11" placeholder="Informe o CPF"
                         value="<?php echo isset($dados['usuario']) ? $dados['usuario']->getCpf() : '';?>"/>
                 </div>
 
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="txtTelefone">Telefone:</label>
-                    <input class="form-control" type="text" id="txtTelefone" name="telefone" 
+                    <input type="text" id="txtTelefone" name="telefone" 
                         maxlength="13" placeholder="Informe o telefone"
                         value="<?php echo isset($dados['usuario']) ? $dados['usuario']->getTelefone() : '';?>"/>
                 </div>
 
-                <div class="form-group">
+                <div class="input-wrapper">
                     <label for="dateNascimento">Data de Nascimento:</label>
-                    <input class="form-control" type="date" id="dateNascimento" name="data_nascimento" 
+                    <input type="date" id="dateNascimento" name="data_nascimento" 
                         value="<?php echo isset($dados['usuario']) ? $dados['usuario']->getDataNascimento() : '';?>"/>
                 </div>
 
@@ -72,21 +69,19 @@ require_once(__DIR__ . "/../include/menu.php");
                 <button type="submit" class="btn btn-success">Gravar</button>
                 <button type="reset" class="btn btn-danger">Limpar</button>
             </form>            
-        </div>
 
-        <div class="col-6">
+        <div class="col-12">
             <?php require_once(__DIR__ . "/../include/msg.php"); ?>
-        </div>
     </div>
 
     <div class="row" style="margin-top: 30px;">
         <div class="col-12">
-        <a class="btn btn-secondary" 
+        <a class="btn btn-secondary"
                 href="<?= BASEURL ?>/controller/UsuarioController.php?action=list">Voltar</a>
         </div>
     </div>
 </div>
-
+</section>
 <?php  
 require_once(__DIR__ . "/../include/footer.php");
 ?>
