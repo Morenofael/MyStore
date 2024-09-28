05d854e4486316d61989554b44ecf62d437a7671
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-09-28 15:42:21 -0300

    rm meta tag

--- app/view/include/header.php
+++ app/view/include/header.php
@@ -8,7 +8,6 @@
 <html lang="pt">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo APP_NAME; ?></title>
 
