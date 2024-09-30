5ecd712f804a24ed28f5d28bea8f0d6c17b3550d
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-09-30 15:48:04 -0300

    Add files via upload

--- app/view/home/home.php
+++ app/view/home/home.php
@@ -8,12 +8,12 @@ require_once(__DIR__ . "/../include/menu.php");
 <div class="main">
     <div class="sidebar">
         <ul class="no-decoration">
-            <li><img src="<?=BASEURL?>/view/img/svg/home.svg" alt="Home" class="icon"><a href="">Home</a></li>
+            <li><img src="<?=BASEURL?>/view/img/svg/home.svg" alt="Home" class="icon"><a href="<?= HOME_PAGE ?>">Home</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/bussola.svg" alt="Bússola" class="icon"><a href="">Explorar</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/coracao.svg" alt="Coração" class="icon"><a href="">Curtido</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/carrinho.svg" alt="Carrinho" class="icon"><a href="">Meu carrinho</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/porquinho.svg" alt="Cofre de porquinho" class="icon"><a href="">Vendendo</a></li>
-            <li><img src="<?=BASEURL?>/view/img/svg/perfil.svg" alt="Perfil" class="icon"><a href="">Perfil</a></li>
+            <li><img src="<?=BASEURL?>/view/img/svg/perfil.svg" alt="Perfil" class="icon"><a href="<?=BASEURL?>/controller/UsuarioController.php?action=display&id=<?=$_SESSION[SESSAO_USUARIO_ID]?>">Perfil</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/engrenagem.svg" alt="Engrenagem" class="icon"><a href="">Configurações</a></li>
         </ul>
     </div>
