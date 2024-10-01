fb147cd0928d36cb5059f3b928f78dbdb38355f8
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2024-09-30 23:55:29 -0300

    add a cofrinho

--- app/view/home/home.php
+++ app/view/home/home.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . "/../include/menu.php");
             <li><img src="<?=BASEURL?>/view/img/svg/bussola.svg" alt="Bússola" class="icon"><a href="">Explorar</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/coracao.svg" alt="Coração" class="icon"><a href="">Curtido</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/carrinho.svg" alt="Carrinho" class="icon"><a href="">Meu carrinho</a></li>
-            <li><img src="<?=BASEURL?>/view/img/svg/porquinho.svg" alt="Cofre de porquinho" class="icon"><a href="">Vendendo</a></li>
+            <li><img src="<?=BASEURL?>/view/img/svg/porquinho.svg" alt="Cofre de porquinho" class="icon"><a href="<?=BASEURL?>/controller/BrechoController.php?action=create">Vendendo</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/perfil.svg" alt="Perfil" class="icon"><a href="<?=BASEURL?>/controller/UsuarioController.php?action=display&id=<?=$_SESSION[SESSAO_USUARIO_ID]?>">Perfil</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/engrenagem.svg" alt="Engrenagem" class="icon"><a href="">Configurações</a></li>
         </ul>
