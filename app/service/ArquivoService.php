4a39bca7df838205cbc9ca3ad2c0a1ba62b659c9
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-10-07 14:40:24 -0300

    add suporte multiplos arquivos

--- app/service/ArquivoService.php
+++ app/service/ArquivoService.php
@@ -1,22 +1,25 @@
 <?php
 class ArquivoService{
-    public function salvarImagemProduto($arquivoImg){
-        if($arquivoImg['size'] <= 0) {
-			echo "O campo arquivo não foi enviado!"; 
-			exit;
-		}
+    public function salvarImagemProduto($arquivo){
+        $totalImagens = count($arquivo["name"]);
+        for($i = 0; $i < $totalImagens; $i++){
+            if($arquivo['size'][$i] <= 0) {
+			    echo "O campo arquivo não foi enviado!"; 
+			    exit;
+		    }
 
-		//Captura o nome e a extensão do arquivo
-		$arquivoNome = explode('.', $arquivoImg['name']);
-		$arquivoExtensao = $arquivoNome[1];
+		    //Captura o nome e a extensão do arquivo
+		    $arquivoNome = explode('.', $arquivo['name'][$i]);
+		    $arquivoExtensao = $arquivoNome[1];
 
-		//A partir da extensão, o ideal é gerar um nome único para o arquivo a fim de encontrá-lo depois
-		$nomeArquivoSalvar = "imagem_" . uniqid('', true) . "." . $arquivoExtensao;
-        if(move_uploaded_file($arquivoImg["tmp_name"], PATH_ARQUIVOS . $nomeArquivoSalvar)){
-            echo "deu certo!";
-        }else{
-            echo "O arquivo não pôde ser salvo, e retornou o erro " . $_FILES["imagem"]["erro"];
-            exit;
+		    //A partir da extensão, o ideal é gerar um nome único para o arquivo a fim de encontrá-lo depois
+		    $nomeArquivoSalvar = "imagem_" . uniqid('', true) . "." . $arquivoExtensao;
+            if(move_uploaded_file($arquivo["tmp_name"][$i], PATH_ARQUIVOS . $nomeArquivoSalvar)){
+                echo "deu certo!";
+            }else{
+                echo "O arquivo não pôde ser salvo, e retornou o erro " . $arquivo["error"][$i];
+                exit;
+            }
         }
     }
 }
