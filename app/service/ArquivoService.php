704b2187144b305e8bdfb56ac95ecb9dc92ecd25
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2024-10-08 12:05:41 -0300

    rm sup multiplos arrays

--- app/service/ArquivoService.php
+++ app/service/ArquivoService.php
@@ -1,33 +1,25 @@
 <?php
-require_once(__DIR__ . "/../dao/ImagemDAO.php");
 class ArquivoService{
 
-    private ImagemDAO $imagemDao;
-
-    public function __construct(){
-        $this->imagemDao = new ImagemDAO();
-    }
 //FAZER SALVAR SÒ UM
-    public function salvarImagemProduto($arquivo, $brechoId){
-        $totalImagens = count($arquivo["name"]);
-        for($i = 0; $i < $totalImagens; $i++){
-            if($arquivo['size'][$i] <= 0) {
-			    echo "O campo arquivo não foi enviado!"; 
-			    exit;
-		    }
+public function salvarImagemProduto($arquivo){
+    if($arquivo['size'] <= 0) {
+			  echo "O campo arquivo não foi enviado!"; 
+			  exit;
+		}
 
-		    //Captura o nome e a extensão do arquivo
-		    $arquivoNome = explode('.', $arquivo['name'][$i]);
-		    $arquivoExtensao = $arquivoNome[1];
+    //Captura o nome e a extensão do arquivo
+    $arquivoNome = explode('.', $arquivo['name']);
+		$arquivoExtensao = $arquivoNome[1];
 
-		    //A partir da extensão, o ideal é gerar um nome único para o arquivo a fim de encontrá-lo depois
-		    $nomeArquivoSalvar = "imagem_" . uniqid('', true) . "." . $arquivoExtensao;
-            if(move_uploaded_file($arquivo["tmp_name"][$i], PATH_ARQUIVOS . $nomeArquivoSalvar)){
-                echo "deu certo!";
-            }else{
-                echo "O arquivo não pôde ser salvo, e retornou o erro " . $arquivo["error"][$i];
-                exit;
-            }
+		//A partir da extensão, o ideal é gerar um nome único para o arquivo a fim de encontrá-lo depois
+		$nomeArquivoSalvar = "imagem_" . uniqid('', true) . "." . $arquivoExtensao;
+    if(move_uploaded_file($arquivo["tmp_name"], PATH_ARQUIVOS . $nomeArquivoSalvar)){
+        echo "deu certo!";
+    }else{
+        echo "O arquivo não pôde ser salvo, e retornou o erro " . $arquivo["error"];
+        exit;
         }
     }
 }
+
