7228806e2ebe9896a495403ec286151da9aa76bc
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-10-16 17:46:19 -0300

    add togle sidwebar

--- app/view/home/home.php
+++ app/view/home/home.php
@@ -5,9 +5,11 @@ require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
 ?>
 <link rel="stylesheet" href="<?= BASEURL ?>/public/css/home.css">
+<span id="sidebar-togler" onclick="togleSidebar()">☰</span>
 <div class="main">
     <div class="sidebar">
         <ul class="no-decoration">
+            <li id="sidebar-untogler" onclick="untogleSidebar()"><img src="<?=BASEURL?>/view/img/svg/home.svg" alt="Fechar" class="icon">Home</li>
             <li><img src="<?=BASEURL?>/view/img/svg/home.svg" alt="Home" class="icon"><a href="<?= HOME_PAGE ?>">Home</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/bussola.svg" alt="Bússola" class="icon"><a href="">Explorar</a></li>
             <li><img src="<?=BASEURL?>/view/img/svg/coracao.svg" alt="Coração" class="icon"><a href="">Curtido</a></li>
