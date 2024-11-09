7b01dbe91e7e834532025c5f400bdc4fdc93abbd
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2024-11-09 20:43:58 -0300

    add pedidos.js

---
+++ app/public/js/pedido.js
@@ -0,0 +1,31 @@
+const BASE_URL = document.getElementById('ipnBaseUrl').value;
+const selEndereco = document.getElementById('selEndereco');
+const btnSalvarEndereco = document.getElementById('btnSalvarEndereco');
+const btnSalvarComprovante = document.getElementById('btnSalvarComprovante');
+const idPedido = document.getElementById('pedidoId').value;
+let idEnderecoEntrega = document.getElementById('idEnderecoEntrega').value;
+let caminhoComprovante = document.getElementById('caminhoComprovante').value;
+function salvarEndereco() {
+    idEnderecoEntrega = selEndereco.value; 
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST",
+                BASE_URL + "/controller/PedidoController.php?action=updateIdEndereco");
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhttp.onload = function() {
+        var json = xhttp.responseText;
+        if(json == "")
+            verificarCampos();
+    }
+
+    xhttp.send("idEndereco=" + encodeURIComponent(idEnderecoEntrega) +  "&idPedido=" + encodeURIComponent(idPedido));
+}
+
+function verificarCampos(){
+    if(idEnderecoEntrega){
+        selEndereco.setAttribute("disabled", "disabled");
+        btnSalvarEndereco.setAttribute("disabled", "disabled");
+    }
+}
+verificarCampos();
