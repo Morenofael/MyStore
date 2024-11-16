3c889b2a3e94a5f0da06de3f48277eff6575732c
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-11-16 14:40:56 -0300

    mudar layout

--- app/view/pedido/pedido.php
+++ app/view/pedido/pedido.php
@@ -6,22 +6,19 @@ require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
 $pedido = $dados["pedido"];
 $imagens = $dados["imagem"];
-echo "<pre>";
-print_r($pedido);
-echo "</pre>";
 ?>
 <link rel="stylesheet" href="<?=BASEURL?>/public/css/pedido.css" media="all">
 <section class="main">
         <div class="esquerda">
-            <img src="<?=BASEURL?>/view/img/upl_img/<?=$imagens[0]->getArquivoNome()?>" alt="">
+            <img src="<?=BASEURL?>/view/img/upl_img/<?=$imagens[0]->getArquivoNome()?>" id="main-img">
             <h3><?=$pedido->getProduto()->getNome()?></h3>
             <h4><?= $pedido->getProduto()->getDescricao()?></h4>
-            <h4><?= $pedido->getProduto()->getPrecoReais()?></h4> 
-            <h4>Gênero:<?= $dados["generoString"]?></h4> 
+            <h4><?= $pedido->getProduto()->getPrecoReais()?></h4>
+            <h4>Gênero:<?= $dados["generoString"]?></h4>
         </div>
         <div class="direita">
             <div id="comprovante-container">
-                <label for="selEndereco">Slecione o endereço de entrega:</label>
+                <label for="selEndereco">Selecione o endereço de entrega:</label>
                 <select id="selEndereco" name="endereco">
                     <option value="">Selecione</option>
                     <?php foreach($dados["enderecosComprador"] as $en): ?>
@@ -29,17 +26,16 @@ echo "</pre>";
                         <?php if($pedido && $pedido->getIdEnderecoEntrega() && $pedido->getIdEnderecoEntrega() == $en->getId()):
                         ?>
                         selected
-                        <?php endif; ?> 
+                        <?php endif; ?>
                         ><?=$en->getCep() . ", " . $en->getLogradouro() . ", " . $en->getNumero()?></option>
                     <?php endforeach; ?>
                 </select>
                 <button onclick="if(confirm('Tem certeza que deseja salver este endereço?'))salvarEndereco()" id="btnSalvarEndereco">salvar</button>
                <br>
-        
+
                 <label for="fileComprovante">Insira o comprovante PIX:</label>
-                <input name="file[]" type="file" id="fileComprovante" >
-                <button onclick="if(confirm('Tem certeza que deseja salver este comprovante?'))salvarComprovante()" id="btnSalvarComprovante">salvar</button>
-<textarea id="upload_file" name="" cols="30" rows="10"></textarea>
+                <input name="file[]" type="file" id="fileComprovante" disabled>
+                <button onclick="if(confirm('Tem certeza que deseja salver este comprovante?'))salvarComprovante()" id="btnSalvarComprovante" disabled>salvar</button>
             </div>
         </div>
     </div>
