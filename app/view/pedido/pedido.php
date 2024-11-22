47c4ce572bd4dc9207b37c4fa1d58c4fc924cfa3
Morenofael <91558374+Morenofael@users.noreply.github.com>
2024-11-21 21:22:56 -0300

    Add files via upload

--- app/view/pedido/pedido.php
+++ app/view/pedido/pedido.php
@@ -9,34 +9,36 @@ $imagens = $dados["imagem"];
 ?>
 <link rel="stylesheet" href="<?=BASEURL?>/public/css/pedido.css" media="all">
 <section class="main">
-        <div class="esquerda">
-            <img src="<?=BASEURL?>/view/img/upl_img/<?=$imagens[0]->getArquivoNome()?>" id="main-img">
-            <h3><?=$pedido->getProduto()->getNome()?></h3>
-            <h4><?= $pedido->getProduto()->getDescricao()?></h4>
-            <h4><?= $pedido->getProduto()->getPrecoReais()?></h4>
-            <h4>Gênero:<?= $dados["generoString"]?></h4>
-        </div>
-        <div class="direita">
-            <div id="comprovante-container">
-                <label for="selEndereco">Selecione o endereço de entrega:</label>
-                <select id="selEndereco" name="endereco">
-                    <option value="">Selecione</option>
-                    <?php foreach($dados["enderecosComprador"] as $en): ?>
-                    <option value="<?=$en->getId()?>"
-                        <?php if($pedido && $pedido->getIdEnderecoEntrega() && $pedido->getIdEnderecoEntrega() == $en->getId()):
-                        ?>
-                        selected
-                        <?php endif; ?>
+    <div class="esquerda">
+        <img src="<?=BASEURL?>/view/img/upl_img/<?=$imagens[0]->getArquivoNome()?>" id="main-img" alt="">
+        <h3><?=$pedido->getProduto()->getNome()?></h3>
+        <h4><?= $pedido->getProduto()->getDescricao()?></h4>
+        <h4><?= $pedido->getProduto()->getPrecoReais()?></h4>
+        <h4>Gênero:<?= $dados["generoString"]?></h4>
+    </div>
+    <div class="direita">
+        <?php if($pedido && $pedido->getComprador()->getId() == $_SESSION[SESSAO_USUARIO_ID]):?>
+        <div id="comprovante-container">
+            <label for="selEndereco">Selecione o endereço de entrega:</label>
+            <select id="selEndereco" name="endereco">
+                <option value="">Selecione</option>
+                <?php foreach($dados["enderecosComprador"] as $en): ?>
+                <option value="<?=$en->getId()?>"
+                    <?php if($pedido && $pedido->getIdEnderecoEntrega() && $pedido->getIdEnderecoEntrega() == $en->getId()):
+                    ?>
+                    selected
+                    <?php endif; ?>
                         ><?=$en->getCep() . ", " . $en->getLogradouro() . ", " . $en->getNumero()?></option>
                     <?php endforeach; ?>
-                </select>
-                <button onclick="if(confirm('Tem certeza que deseja salver este endereço?'))salvarEndereco()" id="btnSalvarEndereco">salvar</button>
-               <br>
+            </select>
+            <button onclick="if(confirm('Tem certeza que deseja salver este endereço?'))salvarEndereco()" id="btnSalvarEndereco">salvar</button>
+            <br>
 
-                <label for="fileComprovante">Insira o comprovante PIX:</label>
-                <input name="file[]" type="file" id="fileComprovante" disabled>
-                <button onclick="if(confirm('Tem certeza que deseja salver este comprovante?'))salvarComprovante()" id="btnSalvarComprovante" disabled>salvar</button>
-            </div>
+            <label for="fileComprovante">Insira o comprovante PIX:</label>
+            <input name="file[]" type="file" id="fileComprovante" disabled>
+            <button onclick="if(confirm('Tem certeza que deseja salver este comprovante?'))salvarComprovante()" id="btnSalvarComprovante" disabled>salvar</button>
+        </div>
+        <?php endif;?>
             <div id="status-display">
                 <span>Status do pedido: <?=$pedido->getStatus()?></span>
             </div>
@@ -48,8 +50,16 @@ $imagens = $dados["imagem"];
                 </select>
                 <button onclick="if(confirm('Tem certeza que deseja alterar o status?'))alterarStatusPedido(document.getElementById('selStatus').value)" id="btnSalvarComprovante">salvar</button>
             <?php endif;?>
+            <?php if($pedido && $pedido->getComprador()->getId() == $_SESSION[SESSAO_USUARIO_ID] && $pedido->getStatus() != 'AI' && $pedido->getStatus() != 'ENT'):?>
+                <button onclick="if(confirm('Tem certeza que deseja confirmar o recebimento?'))alterarStatusPedido('ENT')">Confirmar recebimento</button>
+            <?php endif;?>
+            <?php if($pedido && $pedido->getCaminhoComprovante()):?>
+                <div>
+                    <span>Comprovante PIX:</span><br>
+                    <img src="<?=BASEURL?>/view/img/upl_img/<?=$pedido->getCaminhoComprovante()?>" id="img-comprovante-pix" alt="">
+                </div>
+            <?php endif;?>
         </div>
-    </div>
 </section>
 <input type="hidden" value="<?=$pedido->getId()?>" id="pedidoId">
 <input type="hidden" value="<?=$pedido->getStatus()?>" id="pedidoStatus">
