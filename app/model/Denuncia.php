f646be7c3fcaf3abee18ad3bfe58d048a83b4636
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2024-12-10 21:50:17 -0300

    add model denuncia

---
+++ app/model/Denuncia.php
@@ -0,0 +1,122 @@
+<?php 
+#Nome do arquivo: Denuncia.php
+#Objetivo: classe Model para Denuncia 
+
+require_once(__DIR__ . "/Pedido.php");
+
+class Denuncia implements JsonSerializable {
+
+    private ?int $id;
+    private ?Pedido $pedido;
+    private ?string $status;
+    private ?string $caminhoImagem;
+    private ?string $texto;
+
+    public function jsonSerialize(): array {
+        return array("id" => $this->id,
+                     "pedido" => $this->pedido,
+                     "status" => $this->status,
+                     "caminhoImagem" => $this->caminhoImagem,
+                     "texto" => $this->texto);
+    }
+    
+    /**
+     * Get id.
+     *
+     * @return id.
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Set id.
+     *
+     * @param id the value to set.
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * Get pedido.
+     *
+     * @return pedido.
+     */
+    public function getPedido()
+    {
+        return $this->pedido;
+    }
+    
+    /**
+     * Set pedido.
+     *
+     * @param pedido the value to set.
+     */
+    public function setPedido($pedido)
+    {
+        $this->pedido = $pedido;
+    }
+     
+     /**
+      * Get status.
+      *
+      * @return status.
+      */
+     public function getStatus()
+     {
+         return $this->status;
+     }
+     
+     /**
+      * Set status.
+      *
+      * @param status the value to set.
+      */
+     public function setStatus($status)
+     {
+         $this->status = $status;
+     }
+     
+     /**
+      * Get caminhoImagem.
+      *
+      * @return caminhoImagem.
+      */
+     public function getCaminhoImagem()
+     {
+         return $this->caminhoImagem;
+     }
+     
+     /**
+      * Set caminhoImagem.
+      *
+      * @param caminhoImagem the value to set.
+      */
+     public function setCaminhoImagem($caminhoImagem)
+     {
+         $this->caminhoImagem = $caminhoImagem;
+     }
+     
+     /**
+      * Get texto.
+      *
+      * @return texto.
+      */
+     public function getTexto()
+     {
+         return $this->texto;
+     }
+     
+     /**
+      * Set texto.
+      *
+      * @param texto the value to set.
+      */
+     public function setTexto($texto)
+     {
+         $this->texto = $texto;
+     }
+}
